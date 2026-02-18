5f0f15b1b3bc43f3e3dc03fd5ae4096398ff3d5f
mika-elaa <177727616+mika-elaa@users.noreply.github.com>
2026-02-18 22:15:26 +0800

    Add alert for nextBtn

--- phone-number.js
+++ phone-number.js
@@ -1,5 +1,18 @@
-const nexticon = document.querySelector("#nexticon");
-nexticon.addEventListener("click", ()=> {
-    alert("Your queue number is 23");
+//function validateForm() {
+//    let x = document.forms["getNumber"]["Phone-number"].value;
+//    if (x == "") {
+  //      alert("Number must be filled out");
+   //     return false;
+    //}
 
+//document.getElementById("nextBtn").addEventListener("click",
+//   function(event){
+//    event.stopImmediatePropagation()
+//});
+
+
+let btn = document.querySelector("#nextBtn");
+
+btn.addEventListener('click', function(event) {
+   alert("If the number is invalid/input is empty, you will not receive any queue updates.");
 });
